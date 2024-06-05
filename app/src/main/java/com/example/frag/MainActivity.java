9b4e9d2a7b15b97a098f23589dec1cce9b9fc933
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-06-05 14:18:03 +0200

    Diamond Berg

---
+++ app/src/main/java/com/example/frag/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.frag;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
+}
---
+++ app/src/main/java/com/example/frag/NotificationsFragment.java
@@ -0,0 +1,26 @@
+package com.example.frag;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView_notifications);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Set your adapter here
+        return view;
+    }
+}
